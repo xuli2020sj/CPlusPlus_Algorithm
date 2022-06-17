7edf57d159d81e5ea5f65076a0b353ba1001c399
xuli2020sj <72131412+xuli2020sj@users.noreply.github.com>
2022-06-17 20:07:47 +0800

    update

--- src/algorithm/Sort.cpp
+++ src/algorithm/Sort.cpp
@@ -10,31 +10,12 @@
 #include <chrono>
 #include <random>
 
+
 using namespace std::chrono;
 
 
 
-void testSort(sortAlgo) {
-    uniform_int_distribution<int> u(0, 1000000);
-    default_random_engine e(1);
-    vector<int> vec;
-    for (int i = 0; i < 10000; i++) {
-        vec.push_back(u(e));
-    }
 
-    steady_clock::time_point t1 = steady_clock::now();
-
-    sortAlgo;
-    sort::merge_sort(vec);
-    steady_clock::time_point t2 = steady_clock::now();
-    duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
-
-    //    for_each(vec.begin(), vec.end(), [](int val) {
-    //        cout << val << endl;
-    //    });
-    assert(sort::validateSort(vec));
-    cout << "merge sort time span: " << duration_cast<milliseconds>(time_span).count() << "ms" << endl;
-}
 
 void testMergeSort() {
     uniform_int_distribution<int> u(0, 1000000);
